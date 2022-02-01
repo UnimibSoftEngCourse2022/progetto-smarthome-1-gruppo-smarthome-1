782299d4b0ee57d74cb9dc202a3b80acce8f04ff
federico <federico.finder@gmail.com>
2022-02-01 23:38:55 +0100

    Aggiunto database e classe user.

--- src/main/java/com/smarthome/SmartHome/SmartHomeApplication.java
+++ src/main/java/com/smarthome/SmartHome/SmartHomeApplication.java
@@ -2,20 +2,14 @@ package com.smarthome.SmartHome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SmartHomeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartHomeApplication.class, args);
 	}
 
-	@GetMapping
-	public String hello(){
-		return "Hello home!";
-	}
+
 
 }
---
+++ src/main/java/com/smarthome/SmartHome/user/UserService.java
@@ -0,0 +1,20 @@
+package com.smarthome.SmartHome.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+//@Component
+@Service
+public class UserService {
+
+    public List<User> getUsers(){
+        return List.of(
+                new User(
+                        1L,
+                        "Lorem",
+                        "lorem@ipsum.com"
+                )
+        );
+    }
+}
