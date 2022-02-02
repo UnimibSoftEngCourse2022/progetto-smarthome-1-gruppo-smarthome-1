f1abe396cee1f291b50bf6bf501861ec3ed04167
federico <federico.finder@gmail.com>
2022-02-02 01:18:03 +0100

    Aggiunti metodi POST, DELETE, PUT di riferimento.

--- src/main/java/com/smarthome/SmartHome/user/UserController.java
+++ src/main/java/com/smarthome/SmartHome/user/UserController.java
@@ -1,9 +1,7 @@
 package com.smarthome.SmartHome.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,23 @@ public class UserController {
     public List<User> getUsers(){
         return userService.getUsers();
 
+    }
+
+    @PostMapping
+    public void registerNewUser(@RequestBody User user){
+        userService.addNewUser(user);
+    }
+
+    @DeleteMapping(path = "{userId}")
+    public void deleteUser(@PathVariable("userId") Long userId){
+        userService.deleteStudent(userId);
+    }
+
+    @PutMapping(path = "{userId}")
+    public void updateUser(@PathVariable("userId") Long userId,
+                           @RequestParam(required = false) String name,
+                           @RequestParam(required = false) String email){
+        userService.updateUser(userId, name, email);
     }
 
 }
