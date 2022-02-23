c3e5056465de4f60bf83ab976fecb522c6307186
federico <federico.finder@gmail.com>
2022-02-23 22:22:36 +0100

    Rimossi alcuni code smell

--- src/main/java/com/smarthome/SmartHome/room/RoomConfig.java
+++ src/main/java/com/smarthome/SmartHome/room/RoomConfig.java
@@ -1,10 +1,7 @@
 package com.smarthome.SmartHome.room;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
 
 @Configuration
 public class RoomConfig {
--- src/main/java/com/smarthome/SmartHome/user/UserConfig.java
+++ src/main/java/com/smarthome/SmartHome/user/UserConfig.java
@@ -1,10 +1,6 @@
 package com.smarthome.SmartHome.user;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class UserConfig {
